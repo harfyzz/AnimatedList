9678cf17eab45e1da13aca657d2ca3437368f262
Afeez <harfyzz@gmail.com>
2026-02-12 13:58:34 +0000

    Initial Commit

---
+++ AnimatedList/AnimatedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedListApp.swift
+//  AnimatedList
+//
+//  Created by Afeez Yunus on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
